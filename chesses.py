ca94435f93d63e4eb5809dd46cccbadfb03ab560
Arkotel <120932616+Arkotel@users.noreply.github.com>
2022-12-18 15:50:04 +0300

    Ferzi
    Ferzi

---
+++ chesses.py
@@ -0,0 +1,117 @@
+import turtle as t
+SIZE=50
+
+import queen_class
+queen8=queen_class.Queen(8)
+res = queen8.get_result()
+#########################################
+def square(x,y,z):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+
+    if z%2==1:
+        t.color('black','darkred')
+    else:
+        t.color('black','white')
+
+    t.begin_fill()
+
+    for i in range(4):
+        t.forward(SIZE)
+        t.left(90)
+
+    t.end_fill()
+
+#########################################
+def rectangle(x,y,w,h):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    t.begin_fill()
+
+    for i in range(2):
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+
+    t.end_fill()
+
+
+def polyline(p):
+    t.penup()
+    t.goto(p[0][0],p[0][1])
+    t.pendown()
+    t.begin_fill()
+    for c in p:
+        t.goto(c[0],c[1])
+    t.end_fill()
+'''Если последовательность p состоит, скажем, из 10 элементов,
+for будет поочерёдно обходить их,
+храня значение текущего элемента в переменной c. И типо таким образом мы
+закрашиваем каждый элемент'''
+
+def queen(x,y,z):
+
+    if z%2==0:
+        t.color('black','darkred')
+    else:
+        t.color('black','white')
+
+    rectangle(x+SIZE*0.1, y+SIZE*0.1, SIZE*0.8, SIZE*0.1)
+    rectangle(x+SIZE*0.15, y+SIZE*0.2, SIZE*0.7, SIZE*0.1)
+
+    p=((x+SIZE*0.15, y+SIZE*0.3),
+       (x+SIZE*0.10, y+SIZE*0.4),
+       (x+SIZE*0.90, y+SIZE*0.4),
+       (x+SIZE*0.85, y+SIZE*0.3))
+    polyline(p)
+
+    p=((x+SIZE*0.10, y+SIZE*0.4),
+       (x+SIZE*0.01, y+SIZE*0.95),
+       (x+SIZE*0.30, y+SIZE*0.4),
+       (x+SIZE*0.50, y+SIZE*0.95),
+       (x+SIZE*0.70, y+SIZE*0.4),
+       (x+SIZE*0.99, y+SIZE*0.95),
+       (x+SIZE*0.9, y+SIZE*0.4))
+    polyline(p)
+#########################################Пешки
+'''def circled(x,y,r):
+    
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    t.begin_fill()
+
+    t.circle(r)
+    
+    t.end_fill()
+
+
+def troop(x,y,z):
+
+    if z%2==0:
+        t.color('black','lightgreen')
+    else:
+        t.color('black','white')
+        
+    circled(x+SIZE*0.5, y+SIZE*0.1, SIZE*0.4)'''
+#########################################
+
+t.tracer(0)
+t.hideturtle()
+for j in range(8):
+    for k in range(8):
+        x=j*SIZE-4*SIZE
+        y=k*SIZE-4*SIZE
+        z=j*7+k
+        square(x,y,z)
+        if res[j][k] == 1:
+            queen(x,y,z)
+
+        #if z%2==1:
+            #queen(x,y,z)
+        #else:
+            #troop(x,y,z)
+t.update()
